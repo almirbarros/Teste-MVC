fac180845978b9f1f7a18554e4dbaa9ee45fd084
Almir <Almir@ALMIR-PC>
2019-06-13 12:19:45 -0300

    Commit 2

--- TesteMVC2/TesteMVC2/Controllers/HomeController.cs
+++ TesteMVC2/TesteMVC2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TesteMVC2.Models;
 
 namespace TesteMVC2.Controllers
 {
@@ -11,6 +12,18 @@ namespace TesteMVC2.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Numero()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult NumeroCalculo(NumeroModel modelo)
+        {
+            modelo.Resultado = modelo.Numero1 + modelo.Numero2;
+            return View(modelo);
         }
 
         public ActionResult About()
---
+++ TesteMVC2/TesteMVC2/Models/NumeroModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteMVC2.Models
+{
+    public class NumeroModel
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public double Resultado { get; set; }
+    }
+}
